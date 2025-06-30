631b0a71b6343fd22c3f2d91736b3b03a5d507c4
Boris <bpechersky@gmx.com>
2025-06-29 17:28:23 -0700

    updated wireMockServer implementation so it works in github Actions

--- src/test/java/com/aiqa/AIClientTest.java
+++ src/test/java/com/aiqa/AIClientTest.java
@@ -1,9 +1,27 @@
 package com.aiqa;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
 public class AIClientTest {
+    private WireMockServer wireMockServer;
+    @BeforeClass
+    public void startMockServer() {
+        wireMockServer = new WireMockServer(8080);
+        wireMockServer.start();
+        configureFor("localhost", 8080);
+
+        stubFor(post(urlEqualTo("/api/chat"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\"response\":\"Hello, how can I help you?\"}")));
+    }
+
 
     @Test
     public void testAskQuestion() {
